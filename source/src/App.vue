cb3dac4d59cd15ef8879cb0c17fd1416db376247
Jana Laukova <jana.laukova5@gmail.com>
2019-03-12 15:47:08 +0100

    init program

---
+++ source/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div id="app">
+    <router-link to="/"> <img src="./assets/logo.png"></router-link>
+    <p>tuto menis to ako to vyzera pr</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #041;
+  margin-top: 10px;
+}
+</style>
