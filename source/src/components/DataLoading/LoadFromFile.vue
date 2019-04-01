264bec6c81a04b02c6d5de3e9e8e91f17db8c586
Jana Laukova <jana.laukova5@gmail.com>
2019-04-01 20:56:20 +0200

    po konzultaciou 7 skypovou

--- source/src/components/DataLoading/LoadFromFile.vue
+++ source/src/components/DataLoading/LoadFromFile.vue
@@ -1,20 +1,108 @@
 <template>
   <div>
-    load from file
-  </div>
+  <div id="file-drag-drop">
+    <form ref="fileForm">
+      <span class="dropFiles">Premiestni a pusti súbor sem!</span>
+    </form>
 
+  </div>
+  <div v-for="(file, key) in files" class="file-listing">
+    <img class="preview" v-bind:ref="'preview'+parseInt( key )"/>
+    {{ file.name }}
+  </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: "LoadFromFile",
     data() {
-      return {}
+      return {
+        dragAndDropCapable: false,
+        files: []
+      }
     },
-    methods: {}
+    methods: {
+      determineDragAndDropCapable(){
+        /*
+          Create a test element to see if certain events
+          are present that let us do drag and drop.
+        */
+        let div = document.createElement('div');
+        /*
+          Check to see if the `draggable` event is in the element
+          or the `ondragstart` and `ondrop` events are in the element. If
+          they are, then we have what we need for dragging and dropping files.
+
+          We also check to see if the window has `FormData` and `FileReader` objects
+          present so we can do our AJAX uploading
+        */
+        return (( 'draggable' in div ) || ( 'ondragstart' in div && 'ondrop' in div )) && 'FormData' in window && 'FileReader' in window;
+      }
+    },
+    mounted(){
+      /*
+        Determine if drag and drop functionality is capable in the browser
+      */
+      this.dragAndDropCapable = this.determineDragAndDropCapable();
+
+      /*
+        If drag and drop capable, then we continue to bind events to our elements.
+      */
+      if (this.dragAndDropCapable) {
+        /*
+          Listen to all of the drag events and bind an event listener to each
+          for the fileform.
+        */
+        ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach (function (evt) {
+          /*
+            For each event add an event listener that prevents the default action
+            (opening the file in the browser) and stop the propagation of the event (so
+            no other elements open the file in the browser)
+          */
+          this.$refs.fileForm.addEventListener(evt, function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+          }.bind(this), false);
+        }.bind(this));
+
+        /*
+          Add an event listener for drop to the form
+        */
+        this.$refs.fileForm.addEventListener('drop', function(e) {
+          /*
+            Capture the files from the drop event and add them to our local files
+            array.
+          */
+          for (let i = 0; i < e.dataTransfer.files.length; i++) {
+            this.files.push(e.dataTransfer.files[i]);
+          }
+        }.bind(this));
+      }
+    },
   }
 </script>
 
 <style scoped>
+  form {
+    display: block;
+    height: 300px;
+    width: 400px;
+    background: #afcc99;
+    margin: auto;
+    text-align: center;
+    line-height: 300px;
+    border-radius: 4px;
+  }
 
+  div.file-listing{
+    width: 400px;
+    margin: auto;
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  div.file-listing img{
+    height: 100px;
+  }
 </style>
--- source/src/pages/HomePage.vue
+++ source/src/pages/HomePage.vue
@@ -1,38 +1,41 @@
 <template>
   <div>
+    <h3 class="welcomeText">Vitajte na stránke <br>
+      pre interaktívnu vizualizáciu dát <br>
+      získaných z algoritmov <br>
+      fuzzifikácie a fuzzy zhlukovania.</h3>
+
+    <router-link  to="/dataInput">
+      <b-button id="tlacidko" variant="success">Začni s vizualizáciou..</b-button>
+    </router-link>
     <b-container class="">
       <b-row>
         <b-col offset-lg="2" offset-md="2" md="8" lg="8">
           <b-carousel
             id="carousel-1"
             v-model="slide"
-            :interval="2000"
+            :interval="5000"
             controls
             indicators
             background=""
             img-width="1024"
             img-height="480"
-            style="text-shadow: 1px 1px 2px #333;"
+            style="text-shadow: 1px 1px 2px #f31af3;"
             @sliding-start="onSlideStart"
             @sliding-end="onSlideEnd"
           >
-            <!-- Text slides with image -->
             <b-carousel-slide
-              caption="First slide"
-              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+              caption="Vizualizuj"
+              text="Fuzzy zhlukovanie"
               img-src="https://picsum.photos/1024/480/?image=52"
             ></b-carousel-slide>
 
             <!-- Slides with custom text -->
             <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-              <h1>Hello world!</h1>
+              <h1>Fuzzifikacia!</h1>
             </b-carousel-slide>
 
-            <!-- Slides with image only -->
-            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
 
-            <!-- Slides with img slot -->
-            <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
             <b-carousel-slide>
               <img
                 slot="img"
@@ -43,53 +46,20 @@
                 alt="image slot"
               >
             </b-carousel-slide>
-
-            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-            <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-                a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-              </p>
-            </b-carousel-slide>
           </b-carousel>
-          <!--<p class="mt-4">
-            Slide #: {{ slide }}<br>
-            Sliding: {{ sliding }}
-          </p>-->
         </b-col>
       </b-row>
     </b-container>
-
-    <!--<img src="../assets/logo.png" alt="logo">-->
-    <p class="welcomeText">Vitajte na stránke pre interaktívnu vizualizáciu dát získaných z algoritmov fuzzifikácie a
-      fuzzy zhlukovania.</p>
-    <router-link to="/dataInput">
-      <b-button variant="success">Začni s vizualizáciou..</b-button>
-    </router-link>
-    <!--<b-button v-for='page in pages' v-on:click='currentPage = page' :key="page.id">
-      {{ page.replace('Page', '') }}
-    </b-button>
-    <component v-bind:is='currentPage'></component>-->
   </div>
 </template>
 
 <script>
-  import FuzzyClusteringPage from "@/pages/FuzzyClusteringPage";
-  import HelpPage from "@/pages/HelpPage";
-  import StatisticsPage from "@/pages/StatisticsPage";
-  import DataInputPage from "@/pages/DataInputPage";
-  import FuzzificationPage from "@/pages/FuzzificationPage";
-  import MethodsPage from "@/pages/MethodsPage";
-
   export default {
     name: "HomePage",
-    components: {MethodsPage, FuzzificationPage, DataInputPage, StatisticsPage, HelpPage, FuzzyClusteringPage},
     data() {
       return {
         slide: 0,
         sliding: null,
-        currentPage: 'DataInputPage',
-        pages: ['MethodsPage', 'FuzzificationPage', 'DataInputPage', 'StatisticsPage', 'HelpPage', 'FuzzyClusteringPage'],
       }
     },
     methods: {
@@ -104,6 +74,13 @@
 </script>
 
 <style scoped>
+  h3{
+    padding-top: 50px;
+    margin-bottom: 70px;
+  }
+  #tlacidko{
+    margin-bottom: 50px;
+  }
   /*img {
     margin-top: 5%;
     width: 400px;
